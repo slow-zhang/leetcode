03ae79938a0d6022fb486c6212ed9fd9e1193264
“zjuMrzhang” <377636000@qq.com>
2019-01-07 11:51:44 +0800

    add tree def

--- util.h
+++ util.h
@@ -5,9 +5,9 @@
 
 using namespace std;
 
- struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- };
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
